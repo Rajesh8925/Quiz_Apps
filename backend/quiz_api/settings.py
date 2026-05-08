6a367ef92ae785ac2501bae171877d2451477242
Rajesh8925 <146183163+Rajesh8925@users.noreply.github.com>
2026-05-08 11:20:07 +0530

    fix cors vercel url

--- backend/quiz_api/settings.py
+++ backend/quiz_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fp2-m_^wwzeeo$5ews2jo1i-)t_%z6aq_e^3&3k8r=%_mk473e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1',  'quiz-apps-3.onrender.com']
 
@@ -146,6 +146,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3003",
     "http://localhost:3004",
     "http://localhost:3005",
+    "https://quiz-apps-4.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
