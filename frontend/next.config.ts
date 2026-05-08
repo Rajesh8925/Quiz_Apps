a6d47d2233191c0d6b3495c72b9a311214a3cdf6
Rajesh8925 <146183163+Rajesh8925@users.noreply.github.com>
2026-05-08 17:51:04 +0530

    update api url

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://quiz-apps-my-7jy8.onrender.com/api';
 
 const nextConfig: NextConfig = {
   env: {
